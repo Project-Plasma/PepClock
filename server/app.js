06c86f56a33cd92fc3528c084179fe5303efbd71
Aaron Xavier <aarondxavier@gmail.com>
2017-07-10 11:07:05 -0700

    Adding SSL redirect.

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const middleware = require('./middleware');
 const routes = require('./routes');
+const sslRedirect = require('heroku-ssl-redirect');
 
 const app = express();
 
+app.use(sslRedirect(['production'], 301));
 app.use(middleware.morgan('dev'));
 app.use(middleware.cookieParser());
 app.use(middleware.bodyParser.urlencoded({extended: false}));
